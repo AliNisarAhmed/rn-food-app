102eef7ed23c3612c8497a5fe40a184fa58b5a08
Ali Ahmed <ali.ahmed@punchcard.io>
2019-09-10 20:04:46 -0600

    using axios to make api requests

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
+import YelpAPI from '../api/yelpApi';
 
 const SearchScreen = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+  const [results, setResults] = React.useState([]);
+
+  const searchApi = async () => {
+    const res = await YelpAPI.getSearchResults('/search', {
+      limit: 50,
+      term: searchTerm,
+      location: 'edmonton',
+    });
+    setResults(res.data.businesses);
+  };
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} onTermSubmit={searchApi} />
       <Text>Search Screen</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
